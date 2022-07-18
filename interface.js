2a5fef3a1320ecd0b1effcd3d0cadf2efa9477f1
NEvgen63 <91946609+NEvgen63@users.noreply.github.com>
2022-07-18 15:54:19 +0400

    33

---
+++ interface.js
@@ -0,0 +1,11 @@
+const btnCreate = document.querySelector('.btn-create');
+
+btnCreate.addEventListener ('click', () => {
+    let partnerName = document.querySelector('.partner-name > input').value;
+    console.log(partnerName)
+    //partner = partnerName
+    return partner
+})
+
+console.log(partnerName);
+
