49b12505ecb91fd43189b42f279e950dc7ecfd8e
NEvgen63 <91946609+NEvgen63@users.noreply.github.com>
2022-07-31 22:55:47 +0400

    without electron

---
+++ config.ts
@@ -0,0 +1,17 @@
+
+export let partnerName = "жизнь март";
+export let host = "autotest";
+export let clientId = "autotest";
+export let clientSecret = "autotest";
+export let originId = "333";
+
+// export const creds = {
+//     partnerName: "autotest name",
+//     host: "",
+//     clientId: "",  
+//     clientSecret: "",
+//     originId: "",
+// }
+
+
+// 
--- test/specs/createGroup.ts
+++ test/specs/createGroup.ts
@@ -1,102 +1,120 @@
 import { assert } from "console"
-const credentials = require('../../interfaceApp');
-const { exec } = require('child_process');
+const config = require('../../config')
+
 
 describe("Create place", () => {
-    it("create group", async () => {
-        
-        await browser.url("https://admin.eda.tst.yandex-team.ru/place_groups/add")
-        await $('#username').setValue('admin@yandex-team.ru')
-        await $('#password').setValue('password')
+    it("avtorization", async () => {
+        await browser.url("https://admin.eda.tst.yandex-team.ru/login")
+        //await $('#username').setValue('admin@yandex-team.ru')
+        //await $('#password').setValue('password')
         await $('button[name=_submit]').click()
-
-        await $('#place_group_name').setValue(credentials.partnerName)
-        await $('#place_group_organizationId').setValue('1')
-
-        await $('#place_group_menuParserName').selectByVisibleText('Retail API')
-        await $('#place_group_integrationEngine').selectByVisibleText('Yandex.Eda API')
-        await $('#place_group_stopListAggregator').selectByVisibleText('Retail API Single Place')
-        await $('#place_group_integrationProcessToUseClientCategories').selectByVisibleText('Да')
-        await $('#place_group_useClientCategoriesSynchronizationWithMenu').click()
-
-        await $('#place_group_vendorHost').setValue(credentials.host)
-        await $('#place_group_clientId').setValue(credentials.clientId)
-        await $('#place_group_clientSecret').setValue(credentials.clientSecret)
-        await $('#place_group_scope').setValue('read write')
-        await $('#place_group_orderUpdate').click()
-        await $('#place_group_cookedStatuses').selectByVisibleText('Заказ приготовлен / READY')
-        await $('#place_group_canceledStatuses').selectByVisibleText('Заказ отменен / CANCELLED')
-        await $('#place_group_apiVersion').selectByVisibleText('V2')
-        await $('#place_group_shouldUpdateCatchweightOrder').click()
-        await $('#place_group_dontStopUpdateOrderWithoutItem').click()
-
-        await $('#place_group_menuParserClientId').setValue(credentials.clientId)
-        await $('#place_group_menuParserClientSecret').setValue(credentials.clientSecret)
-        await $('#place_group_menuParserScope').setValue('read write')
-        await $('#place_group_menuParserVendorHost').setValue(credentials.host)
-        await $('#place_group_menuParserMoveDishesToOthersCategory').click()
-        await $('#place_group_menuParserSupportPricesWithCoins').click()
-        await $('#place_group_menuParserSupportCatchWeightGoods').click()
-        await $('#place_group_menuParserApiVersion').selectByVisibleText('V1')
         
-        //await browser.pause(20000)
+        await $('input[name="login"]').setValue("evgenij-nakus")
+        await $('input[name="passwd"]').setValue("Zuk73003jkl")
+        await $('button[type="submit"]').click()
     })
+    // it("create group", async () => {
+        
+    //     await browser.pause(2000)
+    //     await browser.url("https://admin.eda.tst.yandex-team.ru/place_groups/add")
+    //     await browser.pause(2000)
+    //     await $('#place_group_name').setValue(config.partnerName)
+
+    //     await $('#place_group_organizationId').setValue('1')
+
+    //     await $('#place_group_menuParserName').selectByVisibleText('Retail API')
+    //     await $('#place_group_integrationEngine').selectByVisibleText('Yandex.Eda API')
+    //     await $('#place_group_stopListAggregator').selectByVisibleText('Retail API Single Place')
+    //     await $('#place_group_integrationProcessToUseClientCategories').selectByVisibleText('Да')
+    //     await $('#place_group_useClientCategoriesSynchronizationWithMenu').click()
+
+    //     await $('#place_group_vendorHost').setValue(config.host)
+    //     await $('#place_group_clientId').setValue(config.clientId)
+    //     await $('#place_group_clientSecret').setValue(config.clientSecret)
+    //     await $('#place_group_scope').setValue('read write')
+    //     await $('#place_group_orderUpdate').click()
+    //     await $('#place_group_cookedStatuses').selectByVisibleText('Заказ приготовлен / READY')
+    //     await $('#place_group_canceledStatuses').selectByVisibleText('Заказ отменен / CANCELLED')
+    //     await $('#place_group_apiVersion').selectByVisibleText('V2')
+    //     await $('#place_group_shouldUpdateCatchweightOrder').click()
+    //     await $('#place_group_dontStopUpdateOrderWithoutItem').click()
+
+    //     await $('#place_group_menuParserClientId').setValue(config.clientId)
+    //     await $('#place_group_menuParserClientSecret').setValue(config.clientSecret)
+    //     await $('#place_group_menuParserScope').setValue('read write')
+    //     await $('#place_group_menuParserVendorHost').setValue(config.host)
+    //     await $('#place_group_menuParserMoveDishesToOthersCategory').click()
+    //     await $('#place_group_menuParserSupportPricesWithCoins').click()
+    //     await $('#place_group_menuParserSupportCatchWeightGoods').click()
+    //     await $('#place_group_menuParserApiVersion').selectByVisibleText('V1')
+        
+    //     //await $('.btn-success').click()
+    //     await browser.pause(20000)
+    // })
 
     
-    it("create brend", async () => {
+    // it("create brend", async () => {
         
-        await browser.url("https://admin.eda.tst.yandex-team.ru/brands/add")
-        await $('#username').setValue('admin@yandex-team.ru')
-        await $('#password').setValue('password')
-        await $('button[name=_submit]').click()
+    //     await browser.url("https://admin.eda.tst.yandex-team.ru/brands/add")
+    //     // await $('#username').setValue('admin@yandex-team.ru')
+    //     // await $('#password').setValue('password')
+    //     // await $('button[name=_submit]').click()
        
-        await $('#brand_name').setValue(credentials.partnerName)
-        await $('#brand_businessType').selectByVisibleText('Магазин (сборка партнёра)')
-        await $('.select2-selection__choice__remove').click()
-          
-        await $('.//li[contains(text(), "Магазин")]').click()
-        await $('#brand_isStockSupported').click()
+    //     await $('#brand_name').setValue(config.partnerName)
+    //     await browser.pause(10000)
 
-        await $('#brand_categoryType').selectByVisibleText('Клиентские категории')
-        await $('#brand_bitSettings_shouldSynchronizeToNomenclatureService').click()
+    //     await $('#brand_businessType').selectByVisibleText('Магазин (сборка партнёра)')
+    //     await $('.select2-selection__choice__remove').click()
+          
+    //     await $('.//li[contains(text(), "Магазин")]').click()
+    //     await $('#brand_isStockSupported').click()
+
+    //     await $('#brand_categoryType').selectByVisibleText('Клиентские категории')
+    //     await $('#brand_bitSettings_shouldSynchronizeToNomenclatureService').click()
 
         
-    })
+    // })
 
     it("create place", async () => {
+
+        await browser.pause(2000)
         
         await browser.url("https://admin.eda.tst.yandex-team.ru/places/add")
-        await $('#username').setValue('admin@yandex-team.ru')
-        await $('#password').setValue('password')
-        await $('button[name=_submit]').click()
+        // await $('#username').setValue('admin@yandex-team.ru')
+        // await $('#password').setValue('password')
+        // await $('button[name=_submit]').click()
 
-        await $('#name').setValue(credentials.partnerName)
+        await $('#name').setValue(config.partnerName)
         await $('#country').selectByVisibleText('Российская Федерация')
-
+        await browser.pause(2000)
         const region = await $('#region > option:nth-child(2)')
         await region.waitForDisplayed(3000, true);
         
         await $('#region').selectByVisibleText('Москва')
         await $('#bitSettings_allowAutoGeneratedZones').click()
         await $('#select2-brand-container').click()
-        await $('.body > span > span > span.select2-search.select2-search--dropdown > input').setValue(credentials.partnerName)
+        //await browser.pause(2000)
+        await $('.//span/span/span[1]/input').setValue(config.partnerName)
+        await browser.pause(2000)
         await browser.keys('Enter')
-
+        await browser.pause(5000)
         await $('#bitSettings_pickupEnabled').click()
-        //по умолчанию наш тип доставки, но потом переделать с выбором
+        //по умолчанию наш тип доставки, но потом переделать с выбором  в конфиге
         
         await $('.//select[@id="address_input"]/following-sibling::span').click()
         await $('.select2-search.select2-search--dropdown > input').setValue("Россия, Москва, улица Гарибальди, 4Г")
+        await browser.pause(2000)
         await $('#select2-address_input-results > li.select2-results__option.select2-results__option--highlighted').click()
 
         await $('#phoneNumbers_0_phone_number').setValue("+73030303030")
         await $('#phoneNumbers_0_type').selectByVisibleText("Автообзвон")
+        await $('#email').setValue("exemple@gmail.com")
         
-        await $('#select2-placeGroup-container').click()
-        await $('.body > span > span > span.select2-search.select2-search--dropdown > input').setValue(credentials.partnerName)
+        await $('span[aria-labelledby="select2-placeGroup-container"]').click()
+        await $('.//span/span/span[1]/input').setValue(config.partnerName)
         await browser.keys('Enter')
 
-        //Ввести id магазина  await $('#originId').setValue("{originId}")
+        await $('#originId').setValue(config.originId)
         await $('#rating').setValue("3,0")
 
         await $('.//select[@id="sales"]/following-sibling::span').click()
@@ -111,46 +129,53 @@ describe("Create place", () => {
         
         await $('.//a[@data-place-id="2"]').click()
         await $('#billing_contractType').selectByVisibleText("Договор")
+        await browser.pause(2000)
+        await $('.btn-success').click()
 
 
         //await assert($('#select2-address_input-container').isDisplayed)
         
-        
-    })
-    let placeId = 198;
-    const todey = new Date
-    const year = todey.getFullYear()
-    const month = todey.getMonth()
-    const date = todey.getDate()
-    const hours = (todey.getHours() - 2) //отнримаем часы для вступления комиссиии в силу
-    const minutes = todey.getMinutes()
+        await browser.pause(25000)
+     });
+    //let placeId = 198;
+    const today = new Date
+    const year = today.getFullYear()
+    const month = today.getMonth()
+    const date = today.getDate()
+    const hours = (today.getHours() - 2) //отнримаем часы для вступления комиссиии в силу
+    const minutes = today.getMinutes()
 
     const zero = (x) => {
     return x < 10 ? x = 0 : x = ""
     }
     const dateForCommission = `${zero(date)}${date}.${zero(month)}${month}.${year} ${zero(hours)}${hours}:${zero(minutes)}${minutes}`
 
-    it("create place", async () => {
-
-        await browser.url(`https://admin.eda.tst.yandex-team.ru/places/${placeId}/edit`)
-
+    it("create place3", async () => {
+        // await browser.pause(2000)
+        // await browser.url(`https://admin.eda.tst.yandex-team.ru/places/${placeId}/edit`)
+        await browser.pause(3000)
         await $('.//a[@href="#commission"]').click()
-        await browser.pause(5000)
-        await $('#place_commission__percent_commission').setValue('10')
+        await browser.pause(3000)
+        
         
         const dataPiker = $('#place_commission__available_from')
         await dataPiker.click()
-        await browser.keys('Delete')
+        //await browser.keys('Delete')
+        await browser.pause(2000)
+        await dataPiker.click()
         await dataPiker.setValue(dateForCommission)
-        await browser.pause(15000)
+        
+        
+        
+        await browser.pause(3000)
         //await $('#place_commission__available_from').click()
-        await $('#place_commission__percent_acquiring_commission').click() //выделил рандомное поля чтоб убрать датапикет
-        await browser.pause(7000)
+        await $('#place_commission__percent_commission').setValue('10') 
+        await browser.pause(3000)
         await $('.btn.btn-success.js-place-commission-create.float-right').click()
-        await $('.//button[@class="btn btn-success"]').click()
+        //await $('.//button[@class="btn btn-success"]').click()
 
-        await $('.//a[@href="/place-delivery-zones/198/list"]').click()
-        await $('.//a[@href="/place-delivery-zones/198/add"]').click()
+        await $('a[href^="/place-delivery-zones"]').click()
+        await $('a[href*="/place-delivery-zones"]').click()
         
         await $('#place_delivery_zone_name').setValue('test_zone')
         await $('#select2-place_delivery_zone_deliveryCondition-container').setValue('МП Москва: Мин. заказ: 600p. Бесплатно от: 0р. [Н+Д]')
@@ -169,5 +194,5 @@ describe("Create place", () => {
         await browser.pause(2000)
 
 
-    })
+   })
 })
